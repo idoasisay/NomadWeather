eaecf7929c63a7a3334e4730351684802ca8207e
idoasisay <ihannah@kakao.com>
2022-12-17 19:40:13 +0900

    todo App practice

---
+++ secondPractice/WorkHardTravelHardApp/App.js
@@ -0,0 +1,149 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  Alert,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Fontisto } from "@expo/vector-icons";
+import { useEffect, useState } from "react";
+import { theme } from "./color";
+
+const STORAGE_KEY = "@toDos";
+
+export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+
+  const onchangeText = payload => setText(payload);
+  const saveToDos = async toSave => {
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+  };
+  const loadToDos = async () => {
+    const s = await AsyncStorage.getItem(STORAGE_KEY);
+    setToDos(JSON.parse(s));
+  };
+
+  useEffect(() => {
+    loadToDos();
+  }, []);
+
+  const addTodo = async () => {
+    if (text === "") return;
+    const newTodos = { ...toDos, [Date.now()]: { text, working } };
+    setToDos(newTodos);
+    await saveToDos(newTodos);
+    setText("");
+  };
+  const deleteToDo = key => {
+    Alert.alert("정말 삭제하시겠습니까?", "sure?", [
+      { text: "cancel", style: "destructive" },
+      {
+        text: "yeeeees",
+        onPress: async () => {
+          const newToDos = { ...toDos };
+          delete newToDos[key];
+          setToDos(newToDos);
+          await saveToDos(newToDos);
+        },
+      },
+    ]);
+    return;
+  };
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TextInput
+          onSubmitEditing={addTodo}
+          onChangeText={onchangeText}
+          value={text}
+          returnKeyType="done"
+          placeholder={working ? "Add a To do" : "where do you wanna go?"}
+          style={styles.input}
+        />
+        <ScrollView>
+          {Object.keys(toDos).map(key =>
+            toDos[key].working === working ? (
+              <View key={key} style={styles.toDo}>
+                <Text style={styles.toDoText}>{toDos[key].text}</Text>
+                <TouchableOpacity onPress={() => deleteToDo(key)}>
+                  <Fontisto name="trash" size={17} color={theme.grey} />
+                </TouchableOpacity>
+              </View>
+            ) : null
+          )}
+        </ScrollView>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
+    color: "white",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginVertical: 20,
+    fontSize: 17,
+  },
+  toDo: {
+    backgroundColor: theme.toDoBg,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 3,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  toDoText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "900",
+  },
+});
